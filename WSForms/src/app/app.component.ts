6ef9062230661b66a4c7ccf8a30814159c78d1ea
luiza sartori <luizacosartori@gmail.com>
2023-04-03 09:10:31 +1000

    Start of HTML Form

--- WSForms/src/app/app.component.ts
+++ WSForms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { formsData } from './formsData';
 import { dataService } from './dataService';
 
@@ -11,17 +11,32 @@ import { dataService } from './dataService';
 export class AppComponent {
   title = 'WSForms';
   therapists: formsData[] = new Array();
-
+  massageForm = new FormGroup({
+    fullName: new FormControl(''),
+    state: new FormControl(''),
+    postCode: new FormControl(''),
+    occupation: new FormControl(''),
+    employer: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    emergencyPhone: new FormControl(''),
+    emergencyContact: new FormControl(''),
+    relationship: new FormControl(''),
+  })
   constructor(private dataService: dataService) {
 
   }
   getData() {
     this.dataService.getPacientData().subscribe(
       (d: any) => {
-          this.therapists=d;
+        this.therapists = d;
       },
       (err: any) => {
 
       });
+  }
+
+  onSubmit(){
+
   }
 }
