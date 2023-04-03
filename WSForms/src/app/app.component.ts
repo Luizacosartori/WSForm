0d4c3a48e797eb4778cc1c6442a7202b3bbd6efe
luiza sartori <luizacosartori@gmail.com>
2023-04-03 19:21:01 +1000

    Comments Of Validation

--- WSForms/src/app/app.component.ts
+++ WSForms/src/app/app.component.ts
@@ -12,16 +12,16 @@ export class AppComponent {
   title = 'WSForms';
   therapists: formsData[] = new Array();
   massageForm = new FormGroup({
-    fullName: new FormControl(''),
-    state: new FormControl(''),
-    postCode: new FormControl(''),
-    occupation: new FormControl(''),
-    employer: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    emergencyPhone: new FormControl(''),
-    emergencyContact: new FormControl(''),
-    relationship: new FormControl(''),
+    fullName: new FormControl(''), //must have space caracter
+    state: new FormControl(''), //Must have more than 2 caractrers
+    postCode: new FormControl(''), //must be onlye number
+    occupation: new FormControl(''), //Must have more than 2 caractrers
+    employer: new FormControl(''), //Must have more than 2 caractrers
+    email: new FormControl(''), //Must have @
+    phone: new FormControl(''), //must only bu numbers
+    emergencyPhone: new FormControl(''), //must only bu numbers
+    emergencyContact: new FormControl(''), //Must have space
+    relationship: new FormControl(''), //Must have more than 2 caractrers
   })
   constructor(private dataService: dataService) {
 
@@ -39,4 +39,5 @@ export class AppComponent {
   onSubmit(){
 
   }
+
 }
