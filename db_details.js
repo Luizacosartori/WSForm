332ba956bf41f3b62384c23013e1b4a6c535d50b
Rodrigo Valadao <rodrigomarciosv@gmail.com>
2023-04-03 21:55:18 +1000

    favicon update
    Logo favicon updated

--- db_details.js
+++ db_details.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database : "WSForms"
-  };
+  host: "localhost",
+  user: "root",
+  password: "12345678",
+  database: "WSForms",
+};
