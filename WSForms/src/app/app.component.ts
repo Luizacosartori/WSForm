e9a0c6e28fed97a81f9606ebeaddc22f5b6c85cc
Rodrigo Valadao <rodrigomarciosv@gmail.com>
2023-04-06 17:16:32 +1000

    First Form - Incomplete

--- WSForms/src/app/app.component.ts
+++ WSForms/src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 import { formsData } from './formsData';
 import { dataService } from './dataService';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'WSForms';
   therapists: formsData[] = new Array();
   massageForm = new FormGroup({
     fullName: new FormControl(''), //must have space caracter
+    dob: new FormControl(''), //date of birthday
+    city: new FormControl(''),
     state: new FormControl(''), //Must have more than 2 caractrers
     postCode: new FormControl(''), //must be onlye number
     occupation: new FormControl(''), //Must have more than 2 caractrers
@@ -22,22 +29,17 @@ export class AppComponent {
     emergencyPhone: new FormControl(''), //must only bu numbers
     emergencyContact: new FormControl(''), //Must have space
     relationship: new FormControl(''), //Must have more than 2 caractrers
-  })
-  constructor(private dataService: dataService) {
+  });
+  constructor(private dataService: dataService) {}
 
-  }
   getData() {
     this.dataService.getPacientData().subscribe(
       (d: any) => {
         this.therapists = d;
       },
-      (err: any) => {
-
-      });
+      (err: any) => {}
+    );
   }
 
-  onSubmit(){
-
-  }
-
+  onSubmit() {}
 }
