43763a3130202c7ef3fa51a45f7bec717313dfe9
luiza sartori <luizacosartori@gmail.com>
2023-05-18 10:11:26 +1000

    login bug fix

--- WSForms/src/app/login/login.component.ts
+++ WSForms/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { dataService } from '../dataService';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,21 +9,30 @@ import { dataService } from '../dataService';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private dataService: dataService) {}
+  constructor(private dataService: dataService, private router: Router) { }
   hide = true;
   loginForm = new FormGroup({
     username: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required)
   });
-  
+
   login() {
-    this.dataService.getUserToken(this.loginForm.value).subscribe(
-      (d: any) => {
-        localStorage.setItem('user', d.username);
-      },
-      (err: any) => {
-        console.log("Error retrieving the data.",err);
-      }
-    );
+   
+    if (this.loginForm.valid) {
+      alert("Welcome!");
+      this.dataService.getUserToken(this.loginForm.value).subscribe(
+        (d: any) => {
+          localStorage.setItem('user', d.username);
+        },
+        (err: any) => {
+          console.log("Error retrieving the data.", err);
+        }
+      );
+      this.router.navigate(['/home']);
+    } else {
+      alert("All red fields must be filled out!");
+    }
+
   }
+
 }
