7f31aad6cc25462730ef9178131ee858032302a3
Rodrigo Valadao <rodrigomarciosv@gmail.com>
2023-06-06 22:02:24 +1000

    Layout Updates

--- WSForms/src/app/reports/reports.component.ts
+++ WSForms/src/app/reports/reports.component.ts
@@ -1,19 +1,17 @@
 import { Component, ViewChild } from '@angular/core';
 import { dataService } from '../dataService';
 import { massageForm } from '../formsData';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.css']
+  styleUrls: ['./reports.component.css'],
 })
 export class ReportsComponent {
   formsData: massageForm[] = new Array();
-  
 
-  constructor(private dataService: dataService) {
-  }
+  constructor(private dataService: dataService, private router: Router) {}
 
   getData() {
     this.dataService.getMassageFormData().subscribe(
@@ -21,7 +19,16 @@ export class ReportsComponent {
         this.formsData = d;
       },
       (err: any) => {
-        console.log(err)
-      });
+        console.log(err);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+  Logout() {
+    localStorage.removeItem('user');
+    this.router.navigate(['/']);
   }
 }
